7a09bf5fa4ba80fd585473561a8b226723f260ba
Bhargava Ram <bhargavram466@gmail.com>
2026-02-02 12:49:05 -0800

    Add variables and type demonstration in variables.py

---
+++ snippets/variables.py
@@ -0,0 +1,18 @@
+a = 10
+b = 3.5
+c = "machine learning"
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+x = "100"
+y = int(x)
+z = float(x)
+
+print(x, z)
+
+print(bool(0))
+print(bool("python"))
