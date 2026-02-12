8e26631f9daca45d4a2b8c8e2f1b7e1829284761
Bhargava Ram <bhargavram466@gmail.com>
2026-02-12 03:11:24 -0800

    Add functions for square, addition, and greeting

---
+++ snippets/functions.py
@@ -0,0 +1,16 @@
+def square(x):
+    return x * x
+
+
+def add_numbers(a, b):
+    return a + b
+
+
+def greet(name="Ram"):
+    return "Hello " + name
+
+
+print(square(5))
+print(add_numbers(10, 15))
+print(greet())
+print(greet("ML Engineer"))
