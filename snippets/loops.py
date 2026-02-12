f2ee4297e28de9af18c47ee23533ffff0647ad8a
Bhargava Ram <bhargavram466@gmail.com>
2026-02-12 01:34:29 -0800

    Add loops example in loops.py

---
+++ snippets/loops.py
@@ -0,0 +1,23 @@
+# Print numbers 1 to 5
+for i in range(1, 6):
+    print(i)
+
+print("----")
+
+# Sum of numbers in list
+numbers = [5, 10, 15]
+total = 0
+
+for num in numbers:
+    total += num
+
+print("Total:", total)
+
+print("----")
+
+# While loop
+x = 0
+
+while x < 3:
+    print("x is", x)
+    x += 1
