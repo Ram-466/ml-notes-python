6f7d45e6a8bbf172c3385a0e73e8223e7985fc71
Bhargava Ram <bhargavram466@gmail.com>
2026-02-12 07:39:30 -0800

    Create tuples_set.py

---
+++ snippets/tuples_set.py
@@ -0,0 +1,12 @@
+#Tuple example
+point = (10, 20)
+print("Tuple:", point)
+print("First Value:" point[0])
+
+#set example
+numbers = {1, 2, 3, 3, 4, 4, 5}
+print("Set:", numbers)
+
+#Membership check
+if 3 in numbers:
+  print("3 is present")
