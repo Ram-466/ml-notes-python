23f7bd3ee21381ef85845b84e55c3d0b75c16691
Bhargava Ram <bhargavram466@gmail.com>
2026-02-12 05:04:44 -0800

    Create list_comprehensions.py

---
+++ snippets/list_comprehensions.py
@@ -0,0 +1,11 @@
+#Squares from 1 to 5
+squares = [i * i for i in range(1, 6)]
+print("Squares:", squares)
+
+#Even numbers from 1 to 10
+evens = [i for i in range(1,11) if i % 2 == 0]
+
+#convert list of strings to integers
+strings = ["1", "2", "3", "4"]
+numbers = [int(s) for s in string]
+print("Converted:", numbers)
