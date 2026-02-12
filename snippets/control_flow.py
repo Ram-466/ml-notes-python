531b8ed7c7bbc5252005d79fcf1e57fb9e278ac7
Bhargava Ram <bhargavram466@gmail.com>
2026-02-12 01:33:40 -0800

    Add control flow examples in control_flow.py
    Implement control flow examples for even/odd check, grading, and login validation.

---
+++ snippets/control_flow.py
@@ -0,0 +1,27 @@
+x = 15
+
+if x % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+score = 82
+
+if score >= 90:
+    print("Grade A")
+elif score >= 75:
+    print("Grade B")
+elif score >= 60:
+    print("Grade C")
+else:
+    print("Fail")
+
+
+user = "admin"
+password = "1234"
+
+if user == "admin" and password == "1234":
+    print("Login successful")
+else:
+    print("Access denied")
